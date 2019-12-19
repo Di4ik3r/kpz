ec5f75392536653ea6bd5ae885c43c94d179c83b
Di4ik3r <di4iker@gmail.com>
2019-12-19 02:20:54 +0200

    excel export

--- src/main/kotlin/ui/view/panel/ViewMainPanel.kt
+++ src/main/kotlin/ui/view/panel/ViewMainPanel.kt
@@ -1,18 +1,23 @@
 package ui.view.panel
 
-import javafx.beans.binding.Bindings.select
 import me.liuwj.ktorm.dsl.*
 import me.liuwj.ktorm.entity.asSequence
 import me.liuwj.ktorm.entity.count
 import me.liuwj.ktorm.entity.elementAt
 import model.*
+import org.apache.poi.ss.usermodel.*
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.awt.BorderLayout
+import java.io.FileOutputStream
+import java.io.IOException
 import java.math.BigDecimal
 import java.time.LocalDate
+import java.util.*
 import javax.swing.*
 import javax.swing.event.ListSelectionEvent
 import javax.swing.table.DefaultTableColumnModel
 import javax.swing.table.DefaultTableModel
+
 
 class ViewMainPanel : JPanel() {
 
@@ -23,6 +28,8 @@ class ViewMainPanel : JPanel() {
     private val columnsGames = arrayOf("id", "name", "price", "date")
     private val columnsGamesDevelopers = arrayOf("id", "name", "role")
     private val columnsGamesGenres = arrayOf("id", "name")
+
+    private val buttonToExcel = JButton("Export to excel")
 
     init {
         this.layout = BoxLayout(this, BoxLayout.LINE_AXIS)
@@ -45,14 +52,64 @@ class ViewMainPanel : JPanel() {
 
         val panelGames = JPanel(); panelGames.layout = BorderLayout()
         panelGames.add(JScrollPane(tableGames), BorderLayout.CENTER)
+        panelGames.add(buttonToExcel, BorderLayout.SOUTH)
         this.add(panelGames)
         this.add(JScrollPane(tableGamesDevelopers))
         this.add(JScrollPane(tableGamesGenres))
+
+
+        this.buttonToExcel.addActionListener {
+            val workbook: Workbook = XSSFWorkbook()
+            val createHelper = workbook.creationHelper
+
+            val sheet: Sheet = workbook.createSheet("Employee")
+
+            val headerFont: Font = workbook.createFont()
+            headerFont.setBold(true)
+            headerFont.setFontHeightInPoints(14.toShort())
+            headerFont.setColor(IndexedColors.RED.getIndex())
+
+            val headerCellStyle = workbook.createCellStyle()
+            headerCellStyle.setFont(headerFont)
+
+            val headerRow: Row = sheet.createRow(0)
+
+            for (i in 0 until columnsGames.count()) {
+                val cell: Cell = headerRow.createCell(i)
+                cell.setCellValue(columnsGames[i])
+                cell.setCellStyle(headerCellStyle)
+            }
+
+            val dateCellStyle = workbook.createCellStyle()
+            dateCellStyle.dataFormat = createHelper.createDataFormat().getFormat("dd-MM-yyyy")
+
+            var rowNum = 1
+            var games = Games.asSequence()
+            for (game in games) {
+                val row: Row = sheet.createRow(rowNum++)
+                row.createCell(0)
+                    .setCellValue(game.id.toString())
+                row.createCell(1)
+                    .setCellValue(game.name)
+                val dateOfBirthCell: Cell = row.createCell(2)
+                dateOfBirthCell.setCellValue(game.date)
+                dateOfBirthCell.setCellStyle(dateCellStyle)
+                row.createCell(3)
+                    .setCellValue(game.price.toString())
+            }
+
+            for (i in 0 until columnsGames.count()) {
+                sheet.autoSizeColumn(i)
+            }
+
+            val fileOut = FileOutputStream("games.xlsx")
+            workbook.write(fileOut)
+            fileOut.close()
+            
+            workbook.close()
+        }
     }
 
-    fun refreshTable() {
-
-    }
 
     inner class GamesListSelectionListener: DefaultTableColumnModel() {
 
